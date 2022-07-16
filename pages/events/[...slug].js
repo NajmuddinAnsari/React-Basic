4a48a5d1096669d7d2756747ba6d9196c2df6159
Najmuddin Ansari <najmudddin.20jics083@jietjodhpur.ac.in>
2022-07-16 09:16:47 +0530

    completed

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -2,10 +2,17 @@ import { useRouter } from "next/router"
 import EventList from "../../components/events/event-list";
 import { getFilteredEvents } from "../../dummy-data";
 export default function (props) {
-
+    const str = 'najmuddin'
+    console.log(props.events)
+    if (props.events.length === 0) {
+        return (
+            <p>no events fount for the given time</p>
+        );
+    }
     return (
         <div>
-            <EventList items={props.events}  />
+
+            <EventList items={props.events} />
         </div>
     )
 }
@@ -14,11 +21,11 @@ export async function getServerSideProps(context) {
     const fullPaths = params.slug;
     const year = +fullPaths[0];
     const month = +fullPaths[1];
-    
+
     const filteredEVents = getFilteredEvents(year, month);
     return {
         props: {
-            events:filteredEVents
+            events: filteredEVents
         }
     }
 }
